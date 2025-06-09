dcb836bd65ba7b0688a203124e89b593412a477c
Swapneel14 <sarkarswapneel14@gmail.com>
2025-06-09 09:35:06 +0530

    Create Search_In_a_Rotated_Sorted_Array_II.java

---
+++ Search_In_a_Rotated_Sorted_Array_II.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean search(int[] arr, int target) {
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            int mid=(start+end)/2;
+            if(arr[mid]==target)return true;
+            if(arr[start]==arr[mid]&&arr[mid]==arr[end]){
+                start++;
+                end--;
+                continue;
+            }
+           if(arr[start]<=arr[mid]){
+            if(arr[start]<=target&&target<=arr[mid]){
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+           }
+           else{
+            if(arr[mid]<=target&&target<=arr[end]){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+           }
+                  
+                  
+      }
+      return false;
+    }
+}
