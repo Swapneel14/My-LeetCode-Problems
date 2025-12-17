3d34aeed27dc695a4da447a8c217827aa917f7ba
Swapneel14 <sarkarswapneel14@gmail.com>
2025-12-17 23:00:52 +0530

    Delete Stack/LeetCode_2997.java

--- Stack/LeetCode_2997.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int minOperations(int[] nums, int k) {
-        int xor=0;
-        for(int x:nums){
-            xor^=x;
-        }
-        return Integer.bitCount(xor^k);
-    }
-}
