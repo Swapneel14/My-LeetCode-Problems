8af22aad83c13f75f252b9b7f9d50399ce1acb8c
Swapneel14 <sarkarswapneel14@gmail.com>
2025-12-31 20:04:09 +0530

    Create remove_duplicates_from_sorted_LL_ap2.java

---
+++ Linked List/remove_duplicates_from_sorted_LL_ap2.java
@@ -0,0 +1,25 @@
+//Complexity	Value
+//Time	O(n)
+//Space	O(1)
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy=new ListNode(-1);
+        dummy.next=head;
+        ListNode prev=dummy;
+        ListNode curr=head;
+        while(curr!=null){
+          if(curr.next!=null&&(curr.val==curr.next.val)){
+            int val=curr.val;
+            while(curr!=null&&curr.val==val){
+                curr=curr.next;
+            }
+            prev.next=curr;
+          }
+          else{
+            prev=curr;
+            curr=curr.next;
+          }
+        }
+        return dummy.next;
+    }
+}
