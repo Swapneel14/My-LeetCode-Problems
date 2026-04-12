5cfec31eae388de7240a79f0ebceaa0a96147e26
Swapneel14 <sarkarswapneel14@gmail.com>
2026-04-12 23:36:33 +0530

    Create Leetcode_2554.java

---
+++ Greedy/Leetcode_2554.java
@@ -0,0 +1,21 @@
+class Solution {
+   
+   public int maxCount(int[] banned, int n, int maxSum) {
+
+        for (int num : banned) {
+            set.add(num);
+
+        }
+        int sum = 0;
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!set.contains(i) && sum + i <= maxSum) {
+                count++;
+                sum += i;
+            }
+        }
+
+        return count;
+
+    }
+}
